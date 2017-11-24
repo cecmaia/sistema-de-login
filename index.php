65194acf32225e55cd886ff32556283b87a59608
Carlos Eduardo <eduardo@prodam.am.gov.br>
2017-11-24 15:15:34 -0400

    Teste Merger Master

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 Carlos Eduardo
-Juliana Acacia
+
+Nome: DR. CARLOS MAGNO DE SOUZA MEDEIROS - CRM (1412)
+Especialidade: ENDOSCOPIA
+Telefone: (92) 32344816(92) 36330518(92) 36331117(92) 36332843
+Endereço: RUA MARIO YPIRANGA 1039 - ADRIANOPOLIS - MANAUS - AM -
+CEP:69057-001
+Complemento: CLINIMAGEM
+Horário: 5ª,Sábado das 06:00 às 08:00 2ª a 4ª,6ª das 06:00 às 10:00
